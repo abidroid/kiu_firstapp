0426d04eb173d8bc242be7903e87eb754214d7e0
Abid <abidroid1@gmail.com>
2024-09-02 16:18:50 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/abid/kiu_firstapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abid.kiu_firstapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/first_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text(
+          'KIU Project',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true,
+        leading: Icon(Icons.settings),
+      ),
+      body: Text(
+        'Hello World',
+        style: TextStyle(
+            fontSize: 40,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            wordSpacing: 10),
+      ),
+    );
+  }
+}
